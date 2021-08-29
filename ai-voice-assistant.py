c6f11c487319e6ee9eb0849d8d15bae0d0735884
Zied Maaloul <zimaaloul@gmail.com>
2021-08-29 14:58:48 -0700

    update Readme

--- ai-voice-assistant.py
+++ ai-voice-assistant.py
@@ -1,4 +1,4 @@
-import pyttsx3
+import pyttsx3 #pip install pyttsx3
 import datetime
 import speech_recognition as sr #pip install SpeechRecognition
 import wikipedia #pip install wikipedia
@@ -9,36 +9,29 @@ import pyautogui #pip install pyautogui
 import psutil #pip install psutil
 import pyjokes #pip install pyjokes
 
-
-
-# pipwin install pyaudio
-
-engine = pyttsx3.init()
-#voices = engine.getProperty("voices")
-#engine.setProperty("voice",voices[1].id)
-#newVoiceRate = 1500
-#engine.setProperty("rate",newVoiceRate)
+engine = pyttsx3.init() 
+voices = engine.getProperty("voices")
+engine.setProperty("voice",voices[1].id) # Choose the Gneder voice (Male or Female) 0 for male and 1 for female
+newVoiceRate = 150
+engine.setProperty("rate",newVoiceRate) # newVoiceRate is the speech speed
 def speak(audio):
-    engine.say(audio)
+    engine.say(audio) # Speech 
     engine.runAndWait()
 def time():
     Time = datetime.datetime.now().strftime("%I:%M:%S")
     speak(Time)
 
-#speak("this is friday")
-#time()
 def date():
-    year = int(datetime.datetime.now().year)
-    month = int(datetime.datetime.now().month)
-    day = int(datetime.datetime.now().day)
+    year = int(datetime.datetime.now().year) # get Current Year
+    month = int(datetime.datetime.now().month) # get current month
+    day = int(datetime.datetime.now().day) #get current day
     speak("the date is :")
     speak(day)
     speak(month)
     speak(year)
-#date()
 def wishme():
     speak("Welcome Back Sir !")
-    hour = datetime.datetime.now().hour
+    hour = datetime.datetime.now().hour #Get Current Hour
     if hour >= 6 and hour <= 12:
         speak("Good Morning")
     elif  hour >= 12 and hour <= 18:
@@ -47,42 +40,40 @@ def wishme():
         speak("Good evening")
     else:
         speak("Good night")
-    speak("Entellig Assistant at Your Service , How can I Help You ?")
-#wishme()
-
+    speak("Intellig Assistant at Your Service , How can I Help You ?")
 def takeCommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
+    r = sr.Recognizer() #Initialize Recognizer
+    with sr.Microphone() as source: # Make Microphone as Audio Source
         print("Listening...")
         r.pause_threshold = 1
-        audio = r.listen(source)
+        audio = r.listen(source) # Get Speech from Mic
     try:
         print("Recongnizing ....")
-        query = r.recognize_google(audio,language = "en=US")
+        query = r.recognize_google(audio,language = "en=US") # Convert Speech to text (English Language)
     except Exception as e:
         print(e)
         speak("Say that again please ...")
         return "None"
     return query
 def sendmail(to,content):
-    server = smtplib.SMTP("smtp.gmail.com",587)
+    # Configure Email Server
+    server = smtplib.SMTP("smtp.gmail.com",587) # Your smtp server and PORT
     server.ehlo()
     server.starttls()
-    server.login("mail@address.com","mailpassword")
+    server.login("mail@address.com","mailpassword") # Your Email address and password
     server.sendmail("mail@address.com",to,content)
     server.close()
 def screenshot():
-    img = pyautogui.screenshot()
+    img = pyautogui.screenshot() #Take ScreenShot
     imagename = "screennshot-"+str(datetime.datetime.now().day)+"-"+str(datetime.datetime.now().month)+"-"+str(datetime.datetime.now().year)+"-"+datetime.datetime.now().strftime("%I-%M-%S")+".png"
     img.save("C:/Scrren/"+imagename)
-
 def cpu():
-    usage = str(psutil.cpu_percent)
+    usage = str(psutil.cpu_percent) #Get CPU usage Percent
     speak("CPU is at "+ usage+" %")
-    battery = psutil.sensors_battery()
+    battery = psutil.sensors_battery() #Get Battery Percent
     speak("battery is at "+str(battery.percent) +" %")
 def jokes():
-    speak(pyjokes.get_joke())
+    speak(pyjokes.get_joke()) #Get Joke
 if __name__ == "__main__":
     wishme()
     while True:
@@ -95,7 +86,7 @@ if __name__ == "__main__":
             date()
         elif "offline" in query:
             speak("GoodBye")
-            quit()
+            quit() #Exit Programm
         elif "wikipedia" in query:
             speak("Searching")
             query = query.replace("wikipeadia","")
@@ -104,7 +95,7 @@ if __name__ == "__main__":
         elif "send email" in query:
             try:
                 speak("What should i say ?")
-                content = takeCommand()
+                content = takeCommand() #get email content from microphone input(converted to text)
                 to = "zimaaloul@gmail.com"
                 sendmail(to,content)
                 speak("Email Sent Successfully")
@@ -113,9 +104,8 @@ if __name__ == "__main__":
                 speak("Unable to send the message")
         elif "search in web" in query or "search web" in query:
             speak("What should I search")
-            chromepath = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
-            search = takeCommand().lower()
-            print(search)
+            chromepath = "C:/Program Files/Google/Chrome/Application/chrome.exe %s" #Your Browser exe file
+            search = takeCommand().lower() #Get search query from Mic
             wb.get(chromepath).open_new_tab("https://www.google.com/search?q="+search.replace(" ","+"))
         elif "logout" in query:
             os.system("shutdown -l")
@@ -124,17 +114,19 @@ if __name__ == "__main__":
         elif "restart" in query:
             os.system("shutdwn \r \t 1")
         elif "play songs" in query:
-            songsFolder = "C:/Users/zimaa/Desktop/Music"
+            songsFolder = "C:/Users/zimaa/Desktop/Music" #Your Music Directory
             songs = os.listdir(songsFolder)
             os.startfile(os.path.join(songsFolder,songs[0]))
         elif "remember that" in query:
             speak("What should i Remeber ?")
-            data = takeCommand()
+            data = takeCommand() #Remember Order
             speak("you said me to remember "+data)
+            # Save your remember to data.txt
             f = open("data.txt", "a")
             f.write(data+"\n")
             f.close()
         elif "do you know anything" in query:
+            #Read remember from data.txt
             file1 = open('data.txt', 'a')
             Lines = file1.readlines()
             count = 0
---
+++ readme.md
@@ -0,0 +1,50 @@
+# AI Voice Assistant
+## _Artificial Intelligence Voice Assistant By Python_
+
+
+Intellig Assistant is  Voice Assistant , get command from microphone , transform speech to command using Artificial Intelligence and execute the task.
+
+
+## Features
+
+- Get Current date and time
+- Take Screenshot
+- Get CPU and Battery usage percent
+- Search in the web
+- Play Songs
+- Send email
+- Search in Wikipedia
+- Remember memory (Notes)
+- Logout , Shutdown and Restart
+- Jokes
+- Go Offline
+
+
+
+## Installation
+
+Install the dependencies
+
+```sh
+pip install -r requirements.txt
+```
+
+Install PyAudio from Pipwin (Only For Windows)
+
+```sh
+pipwin install pyaudio
+```
+
+## Modules
+
+Intellig Assistant is currently extended with the following modules.
+Instructions and description to install for your application.
+
+| Module | Description | Installation |
+| ------ | ------ |------ |
+| pyttsx3 |speech | pip install pyttsx3 |
+| speech_recognition | Speech to text Recognition |pip install SpeechRecognition |
+| wikipedia | Get Wikipedia results from search query | pip install wikipedia |
+| pyautogui | Take Screenshot | pip install pyautogui |
+| psutil | Get system informations like cpu usage | pip install psutil |
+| pyjokes | Get Jokes list | pip install pyjokes |
